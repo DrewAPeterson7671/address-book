ae545efe7bb94db16a2794b25a84377be1256126
Student <example@example.com>
2019-11-04 09:54:54 -0800

    fix format in js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
+// Business Logic for AddressBook ---------
 function AddressBook() {
   this.contacts = [],
-  this.currentID= 0
+  this.currentId = 0
 }
 
 AddressBook.prototype.addContact = function(contact) {
@@ -9,10 +10,34 @@ AddressBook.prototype.addContact = function(contact) {
 }
 
 AddressBook.prototype.assignId = function() {
-  this.currentID +=1;
-  return this.currentID;
+  this.currentId += 1;
+  return this.currentId;
 }
 
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        return this.contacts[i];
+      }
+    }
+  };
+  return false;
+}
+
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i]) {
+      if (this.contacts[i].id == id) {
+        delete this.contacts[i];
+        return true;
+      }
+    }
+  };
+  return false;
+}
+
+// Business Logic for Contacts ---------
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName,
   this.lastName = lastName,
